e7113b2cdbce6e9993930c3b209002825b8ac3c3
Rayzr522 <rayzr522@gmail.com>
2017-04-15 19:54:16 -0400

    Added Unix installer

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SharpBot-installer
-Home of the SharpBot installer scripts
+Home of the SharpBot installer scripts. Proper usage can be found on the [main SharpBot page](https://github.com/Rayzr522/SharpBot).
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+function fail {
+    echo "$(tput setaf 1)$@$(tput sgr0)"
+    echo -e "\nPlease check https://github.com/Rayzr522/SharpBot/blob/master/FAQ.md before asking for help.\n"
+    exit 1
+}
+
+function say {
+    echo -e "\n$(tput setaf 2)> $(tput sgr0)$@\n"
+}
+
+say "Checking depedencies... "
+
+for dep in node yarn git; do 
+    if [[ ! "$(command -v $dep)" ]]; then
+        fail "$dep is not installed! Please see http://github.com/Rayzr522/SharpBot for instructions on how to install SharpBot."
+    fi
+done
+
+say "Downloading bot..."
+git clone https://github.com/Rayzr522/SharpBot.git || fail "Failed to download bot!"
+
+[[ -d SharpBot ]] || fail "Could not find SharpBot folder!"
+cd SharpBot || fail "Could not enter SharpBot folder!"
+
+say "Installing dependencies..."
+yarn || fail "Failed to install dependencies!"
+
+say "Starting bot..."
+yarn start || fail "Failed to start bot!"
