777124f15d96620c89b8baf0e46e151fa128ab1c
Rayzr <blood314@icloud.com>
2017-05-04 08:32:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function fail {
     echo "$(tput setaf 1)$@$(tput sgr0)"
-    echo -e "\nPlease check https://github.com/Rayzr522/SharpBot/blob/master/FAQ.md before asking for help.\n"
+    echo -e "\nPlease check https://github.com/RayzrDev/SharpBot/blob/master/FAQ.md before asking for help.\n"
     exit 1
 }
 
@@ -13,12 +13,12 @@ say "Checking depedencies... "
 
 for dep in node yarn git; do 
     if [[ ! "$(command -v $dep)" ]]; then
-        fail "$dep is not installed! Please see http://github.com/Rayzr522/SharpBot for instructions on how to install SharpBot."
+        fail "$dep is not installed! Please see http://github.com/RayzrDev/SharpBot for instructions on how to install SharpBot."
     fi
 done
 
 say "Downloading bot..."
-git clone https://github.com/Rayzr522/SharpBot.git || fail "Failed to download bot!"
+git clone https://github.com/RayzrDev/SharpBot.git || fail "Failed to download bot!"
 
 [[ -d SharpBot ]] || fail "Could not find SharpBot folder!"
 cd SharpBot || fail "Could not enter SharpBot folder!"
